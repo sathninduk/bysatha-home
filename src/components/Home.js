bb9f6247a320929ed107c331dbb9edf5d9911003
Sathnindu Kottage <sathnidukottage@gmail.com>
2022-07-18 11:53:56 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -854,6 +854,19 @@ export default class Home extends Component {
                                     </svg></a>
                                 </a>
                                 <a className={"col con-mid block-href"}
+                                   href={"https://www.hackerearth.com/@sathnidukottage"} rel="noreferrer" target={"_blank"}>
+                                    <p className={"edition-name"}>HackerEarth</p>
+                                    <a href={"https://www.hackerearth.com/@sathnidukottage"} rel="noreferrer"
+                                       target={"_blank"}>Visit <svg
+                                        className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiSvgIcon-root MuiSvgIcon-fontSizeLarge css-1shn170"
+                                        focusable="false" aria-hidden="true" viewBox="0 0 24 24" fill="#5765f1"
+                                        height="15" width="15" data-testid="OpenInNewIcon" tabIndex="-1"
+                                        title="OpenInNew">
+                                        <path
+                                            d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path>
+                                    </svg></a>
+                                </a>
+                                <a className={"col con-mid block-href"}
                                    href={"https://gitcoin.co/sathninduk"} rel="noreferrer" target={"_blank"}>
                                     <p className={"edition-name"}>GitCoin</p>
                                     <a href={"https://gitcoin.co/sathninduk"} rel="noreferrer"
